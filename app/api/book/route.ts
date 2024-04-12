4fb9c38d4049084dadcfc6a9cf9e6594a7651f23
Matthew Rey <matthewlemuelr@gmail.com>
2024-04-12 10:39:54 +0800

    Change supabase client

--- app/api/book/route.ts
+++ app/api/book/route.ts
@@ -1,9 +1,9 @@
+import { createClient } from "@/lib/db";
 import {
   BookValidator,
   CreateBookRequestValidator,
   DeleteBookRequestValidator,
 } from "@/lib/validators/book";
-import { createClient } from "@supabase/supabase-js";
 import { z } from "zod";
 
 export async function POST(req: Request) {
@@ -12,10 +12,7 @@ export async function POST(req: Request) {
     const { author, genre, publishedDate, title } =
       CreateBookRequestValidator.parse(body);
 
-    const supabase = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL || "",
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
-    );
+    const supabase = createClient();
 
     const { error } = await supabase
       .from("book")
@@ -39,10 +36,7 @@ export async function PATCH(req: Request) {
     const { id, author, genre, publishedDate, title } =
       BookValidator.parse(body);
 
-    const supabase = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL || "",
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
-    );
+    const supabase = createClient();
 
     const { error } = await supabase
       .from("book")
@@ -66,10 +60,7 @@ export async function DELETE(req: Request) {
     const body = await req.json();
     const { id } = DeleteBookRequestValidator.parse(body);
 
-    const supabase = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL || "",
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
-    );
+    const supabase = createClient();
 
     const { error } = await supabase.from("book").delete().eq("id", id);
     if (error) throw new Error(`Book deletion failed ${error.message}`);
